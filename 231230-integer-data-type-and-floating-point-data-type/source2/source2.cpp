7ff2aab6465b65f85f820aa05a012c421c3371af
kobe <dev.skyachieve91@gmail.com>
2023-12-31 12:34:33 +0900

    :sparkles: Feat: 정수 자료형과 실수 자료형, unsigned 학습.

---
+++ 231230-integer-data-type-and-floating-point-data-type/source2/source2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+
+    // 실수형: 소수부가 있는 수
+
+    float a = 3.14;
+    int c = 3.14;
+
+    cout << a << " " << c << endl;
+
+    return 0;
+}
