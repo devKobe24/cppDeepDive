fcb928c3ba605d364f7046d3d55e7fc44b381c89
kobe <dev.skyachieve91@gmail.com>
2024-01-02 05:22:56 +0900

    :sparkles: Feat: 산술 연산자 학습.

---
+++ 240102-ArithmeticOperrators/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // C++의 연산자 : +, -, *, /, %
+
+    // 아래의 코드에서 피연산자 == 3과 2, 산술연산자 == +
+    int a = 3 + 2;
+
+    cout << a << endl;
+
+    int b = 10;
+    int c = 3;
+
+    int d = b + c;
+    int e = b - c;
+    int f = b * c;
+    int g = b / c;
+    int h = b % c;
+
+    cout << "d : " << d << endl;
+    cout << "e : " << e << endl;
+    cout << "f : " << f << endl;
+    cout << "g : " << g << endl;
+    cout << "h : " << h << endl;
+
+    return 0;
+};
